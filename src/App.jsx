1a94f951b4ac0e8f4143eb480fe179b2edc10460
Gonzalo Manoukian <gonzalomanoukian@gmail.com>
2021-08-24 11:34:00 -0300

    index - app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// Importaciones
+import React from 'react'
+
+// Generacion de la funcion (flecha) del componente
+const App = () => {
+    // Lo que ejecuta la funcion
+    return <h1> Calc App </h1>
+}
+
+// Exportacion
+export default App
+
